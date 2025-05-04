11282273cbe819034deeb025c3f1c95b9961818f
Joel Espinoza <jespinoza522@gmail.com>
2025-05-04 11:20:47 -0500

    Se modifico el componente y los archivos app.module.ts, app-routing.module.ts para agregar el formulario para registro de vehiculos

--- front/src/app/vehiculo-form/vehiculo-form/vehiculo-form.component.ts
+++ front/src/app/vehiculo-form/vehiculo-form/vehiculo-form.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vehiculo-form',
-  imports: [],
+  standalone: true,
   templateUrl: './vehiculo-form.component.html',
-  styleUrl: './vehiculo-form.component.css'
+  styleUrls: ['./vehiculo-form.component.css'],
+  imports: [CommonModule, FormsModule]
 })
 export class VehiculoFormComponent {
+  vehiculo = {
+    placa: '',
+    modelo: '',
+    estado: '',
+    tipo: 'Liviana'
+  };
 
+  registrarVehiculo() {
+    console.log('Vehículo a registrar:', this.vehiculo);
+    alert('Vehículo registrado (ver consola)');
+  }
 }
